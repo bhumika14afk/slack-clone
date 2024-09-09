882890e6343f9b4f5e551c7814676db43869ae0a
bhumika14afk <bhumika.muralidhara@gmail.com>
2024-09-09 23:09:34 +0530

    added config for vercel

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -8,7 +8,7 @@ export const AuthScreen = () => {
 
   return (
     <div className="h-full flex items-center justify-center bg-[#5C3B58]">
-      Auth Screen
+      <div className="md:h-auto md:w-[420px]">Auth Screen</div>
     </div>
   );
 };

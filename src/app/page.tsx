80f3d0254e172ac1f948d168a527f6a8bb75df66
Shashank <shashank@kangasys.com>
2024-09-10 23:14:31 +0530

    revert back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
-  return <div className="h-full flex items-center justify-center bg-[#5C3B58]">
-  <div className="md:h-auto md:w-[420px]">Auth Screen</div>
-</div>;
+  return <AuthScreen />;
 }

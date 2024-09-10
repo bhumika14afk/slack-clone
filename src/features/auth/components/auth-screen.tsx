ef091c93b2ca548f9028fb7c0f75f5a201ac0b51
Shashank <shashank@kangasys.com>
2024-09-10 23:09:06 +0530

    test

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useState } from "react";
-import { SignInFlow } from "../types";
 
 export const AuthScreen = () => {
-  const [state, setState] = useState<SignInFlow>("signIn");
+
 
   return (
     <div className="h-full flex items-center justify-center bg-[#5C3B58]">
